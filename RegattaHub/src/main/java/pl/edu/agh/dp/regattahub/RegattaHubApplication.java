f6b05a91a0a00894cb6193d617b6d48f66e1b99e
Konrad Małek <konrad.malek1@gmail.com>
2026-02-01 18:17:20 +0100

    init commit

---
+++ RegattaHub/src/main/java/pl/edu/agh/dp/regattahub/RegattaHubApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.agh.dp.regattahub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegattaHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegattaHubApplication.class, args);
+    }
+
+}
---
+++ RegattaHub/src/test/java/pl/edu/agh/dp/regattahub/RegattaHubApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.agh.dp.regattahub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegattaHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
