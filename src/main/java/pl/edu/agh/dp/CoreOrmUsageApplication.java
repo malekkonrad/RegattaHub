3d32fb1068d7d65a155e7759b62c89a3514063ca
Konrad Małek <konrad.malek1@gmail.com>
2026-02-05 11:22:00 +0100

    fresh init

---
+++ src/main/java/pl/edu/agh/dp/CoreOrmUsageApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.agh.dp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreOrmUsageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoreOrmUsageApplication.class, args);
+    }
+
+}
